c7c904893e583d51ad2e0f3cfb4c3c6647a59914
Breno Bering <bbering88@gmail.com>
2023-04-22 15:28:11 -0300

    Adicionadas soluções, corrigidos alguns movimentos dos trens

--- control/mainController.java
+++ control/mainController.java
@@ -11,10 +11,13 @@ package control;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import threads.*;
@@ -22,6 +25,20 @@ import threads.*;
 public class mainController implements Initializable {
 
   // Todos os FXML que serao utilizados
+
+  // Botoes especificos de acelerar e frear os trens
+
+  @FXML
+  private Button lessRed;
+
+  @FXML
+  private Button plusRed;
+
+  @FXML
+  private Button plusBlue;
+
+  @FXML
+  private Button lessBlue;
 
   @FXML
   private Button option01;
@@ -47,22 +64,38 @@ public class mainController implements Initializable {
   @FXML
   private ImageView train4;
 
+  @FXML
+  private ComboBox solutionsBox;
+
   // Instanciando ambas as threads (que sao os trens)
 
   private ThreadFirstTrain threadTrain1;
 
   private ThreadSecondTrain threadTrain2;
 
-  // Botao que inicia ambas as threads
+  // Variavel de travamento
 
-  public void setTrainImg(String directory, int imgNum) {
-    if (imgNum == 1) {
-      train1.setImage(new Image(directory));
-    }
-    if (imgNum == 2) {
-      train2.setImage(new Image(directory));
-    }
-  }
+  private int LV1 = 0;
+
+  private int LV2 = 0;
+
+  // Variavel de escolha de display
+
+  private int op;
+
+  // Variaveis de velocidade dos trens que serao alteradas em tempo real
+
+  private long speedRed = 10;
+
+  private long speedBlue = 10;
+
+  // Variaveis usadas na estrita alternancia
+
+  private int turn01 = 1;
+
+  private int turn02 = 0;
+
+  // Inicializando os recursos necessarios para serem utilizados no controller
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
@@ -74,6 +107,39 @@ public class mainController implements Initializable {
     train2.setVisible(false);
     train3.setVisible(false);
     train4.setVisible(false);
+    lessRed.setOnAction(new EventHandler<ActionEvent>() {
+      @Override
+      public void handle(ActionEvent event) {
+        speedRed = speedRed + 1;
+      }
+    });
+    plusRed.setOnAction(new EventHandler<ActionEvent>() {
+      @Override
+      public void handle(ActionEvent event) {
+        if (speedRed == 1)
+          System.out.println("Velocidade maxima atingida!!!");
+        else {
+          speedRed = speedRed - 1;
+        }
+      }
+    });
+    lessBlue.setOnAction(new EventHandler<ActionEvent>() {
+      @Override
+      public void handle(ActionEvent event) {
+        speedBlue = speedBlue + 1;
+      }
+    });
+    plusBlue.setOnAction(new EventHandler<ActionEvent>() {
+      @Override
+      public void handle(ActionEvent event) {
+        if (speedBlue == 1)
+          System.out.println("Velocidade maxima atingida!!!");
+        else {
+          speedBlue = speedBlue - 1;
+        }
+      }
+    });
+    solutionsBox.getItems().addAll("Variavel de Travamento", "Estrita alternancia");
   }
 
   // Metodos que retornam as imagens dos trens, utilizado nas threads
@@ -106,8 +172,10 @@ public class mainController implements Initializable {
     train2.setVisible(true);
     train3.setVisible(false);
     train4.setVisible(false);
-    threadTrain1.run(1);
-    threadTrain2.run(1);
+    train2.setImage(new Image("/assets/blueTrain.png"));
+    threadTrain1.start();
+    threadTrain2.start();
+    this.op = 1;
   }
 
   @FXML
@@ -120,8 +188,10 @@ public class mainController implements Initializable {
     train2.setVisible(false);
     train3.setVisible(true);
     train4.setVisible(true);
-    threadTrain1.run(2);
-    threadTrain2.run(2);
+    train4.setImage(new Image("/assets/blueTrain02.png"));
+    threadTrain1.start();
+    threadTrain2.start();
+    this.op = 2;
   }
 
   @FXML
@@ -132,10 +202,12 @@ public class mainController implements Initializable {
     option04.setVisible(false);
     train1.setVisible(true);
     train2.setVisible(false);
-    train3.setVisible(true);
-    train4.setVisible(false);
-    threadTrain1.run(3);
-    threadTrain2.run(3);
+    train3.setVisible(false);
+    train4.setVisible(true);
+    train4.setImage(new Image("/assets/blueTrain02.png"));
+    threadTrain1.start();
+    threadTrain2.start();
+    this.op = 3;
   }
 
   @FXML
@@ -146,9 +218,260 @@ public class mainController implements Initializable {
     option04.setVisible(false);
     train1.setVisible(false);
     train2.setVisible(true);
-    train3.setVisible(false);
-    train4.setVisible(true);
-    threadTrain1.run(4);
-    threadTrain2.run(4);
+    train3.setVisible(true);
+    train4.setVisible(false);
+    train3.setImage(new Image("/assets/train02.png"));
+    train2.setImage(new Image("/assets/blueTrain.png"));
+    threadTrain1.start();
+    threadTrain2.start();
+    this.op = 4;
+  }
+
+  // Metodo para indicar onde se encontra a zona critica
+
+  public void enterFirstBridge(String train) throws InterruptedException {
+  }
+
+  public void enterSecondBridge(String train) throws InterruptedException {
+  }
+
+  // Metodo que move o trem na direcao X e configura a velocidade X
+
+  public void moveX(String train, int pos, int starterPoint, int side) {
+    if (train == "red" && side == 1) {
+      for (int i = starterPoint; i <= pos; i++) {
+        int moveX = i;
+        Platform.runLater(() -> train1.setX(moveX));
+        try {
+          threadTrain1.sleep(speedRed);
+        } catch (InterruptedException e) {
+        }
+      }
+    }
+    if (train == "blue" && side == 1) {
+      for (int i = starterPoint; i <= pos; i++) {
+        int moveX = i;
+        Platform.runLater(() -> train2.setX(moveX));
+        try {
+          threadTrain2.sleep(speedBlue);
+        } catch (InterruptedException e) {
+        }
+      }
+    }
+    if (train == "red" && side == 2) {
+      for (int i = pos; i <= starterPoint; i++) {
+        int moveX = -i;
+        Platform.runLater(() -> train3.setX(moveX));
+        try {
+          threadTrain1.sleep(speedRed);
+        } catch (InterruptedException e) {
+        }
+      }
+    }
+    if (train == "blue" && side == 2) {
+      for (int i = pos; i <= starterPoint; i++) {
+        int moveX = -i;
+        Platform.runLater(() -> train4.setX(moveX));
+        try {
+          threadTrain2.sleep(speedBlue);
+        } catch (InterruptedException e) {
+        }
+      }
+    }
+  }
+
+  // Metodo que move o trem na direcao Y e configura a velocidade Y
+
+  public void moveY(String train, int pos, int starterPoint, String direc, int zone) {
+    if (direc == "down" && zone == 1) {
+      for (int i = starterPoint; i <= pos; i++) {
+        int moveY = i;
+        if (train == "red")
+          Platform.runLater(() -> train1.setY(moveY));
+        if (train == "blue")
+          Platform.runLater(() -> train2.setY(moveY));
+        try {
+          if (train == "red")
+            threadTrain1.sleep(speedRed);
+        } catch (InterruptedException e) {
+        }
+        if (train == "blue")
+          try {
+            threadTrain2.sleep(speedBlue);
+          } catch (InterruptedException e) {
+          }
+      }
+    }
+    if (direc == "down" && zone == 2) {
+      for (int i = starterPoint; i <= pos; i++) {
+        int moveY = i;
+        if (train == "red")
+          Platform.runLater(() -> train3.setY(moveY));
+        if (train == "blue")
+          Platform.runLater(() -> train4.setY(moveY));
+        try {
+          if (train == "red")
+            threadTrain1.sleep(speedRed);
+        } catch (InterruptedException e) {
+        }
+        if (train == "blue")
+          try {
+            threadTrain2.sleep(speedBlue);
+          } catch (InterruptedException e) {
+          }
+      }
+    }
+    if (direc == "up" && pos > 0 && zone == 1) {
+      for (int i = pos; i >= starterPoint; i--) {
+        int moveY = i;
+        if (train == "red")
+          Platform.runLater(() -> train1.setY(moveY));
+        if (train == "blue")
+          Platform.runLater(() -> train2.setY(moveY));
+        if (train == "red")
+          try {
+            threadTrain1.sleep(10);
+          } catch (InterruptedException e) {
+          }
+        try {
+          if (train == "blue")
+            threadTrain2.sleep(10);
+        } catch (InterruptedException e) {
+        }
+      }
+    } else if (direc == "up" && pos < 0 && zone == 1) {
+      for (int i = starterPoint; i >= pos; i--) {
+        int moveY = i;
+        if (train == "red")
+          Platform.runLater(() -> train1.setY(moveY));
+        if (train == "blue")
+          Platform.runLater(() -> train2.setY(moveY));
+        if (train == "red")
+          try {
+            threadTrain1.sleep(10);
+          } catch (InterruptedException e) {
+          }
+        try {
+          if (train == "blue")
+            threadTrain2.sleep(10);
+        } catch (InterruptedException e) {
+        }
+      }
+    }
+    if (direc == "up" && pos > 0 && zone == 2) {
+      for (int i = pos; i >= starterPoint; i--) {
+        int moveY = i;
+        if (train == "red")
+          Platform.runLater(() -> train3.setY(moveY));
+        if (train == "blue")
+          Platform.runLater(() -> train4.setY(moveY));
+        if (train == "red")
+          try {
+            threadTrain1.sleep(10);
+          } catch (InterruptedException e) {
+          }
+        try {
+          if (train == "blue")
+            threadTrain2.sleep(10);
+        } catch (InterruptedException e) {
+        }
+      }
+    } else if (direc == "up" && pos < 0 && zone == 2) {
+      for (int i = starterPoint; i >= pos; i--) {
+        int moveY = i;
+        if (train == "red")
+          Platform.runLater(() -> train3.setY(moveY));
+        if (train == "blue")
+          Platform.runLater(() -> train4.setY(moveY));
+        if (train == "red")
+          try {
+            threadTrain1.sleep(10);
+          } catch (InterruptedException e) {
+          }
+        try {
+          if (train == "blue")
+            threadTrain2.sleep(10);
+        } catch (InterruptedException e) {
+        }
+      }
+    }
+  }
+
+  // Metodo que rotaciona os trens em tempo real para fazer curvas mais suaves no
+  // trilho
+
+  public void rotate(String train, int rotation, int direc) {
+    if (train == "red" && direc == 1) {
+      Platform.runLater(() -> train1.setRotate(rotation));
+    }
+    if (train == "blue" && direc == 1) {
+      Platform.runLater(() -> train2.setRotate(rotation));
+    }
+    if (train == "red" && direc == 2) {
+      Platform.runLater(() -> train3.setRotate(rotation));
+    }
+    if (train == "blue" && direc == 2) {
+      Platform.runLater(() -> train4.setRotate(rotation));
+    }
+  }
+
+  // Getter do display de trens (as 4 possibilidades)
+
+  public int getOp() {
+    return op;
+  }
+
+  // Getters e Setters para as variaveis de travamento
+
+  public int getLV1() {
+    return LV1;
+  }
+
+  public int getLV2() {
+    return LV2;
+  }
+
+  public void setLV1(int lV1) {
+    LV1 = lV1;
+  }
+
+  public void setLV2(int lV2) {
+    LV2 = lV2;
+  }
+
+  // Getters utilizados para a estrita alternancia
+
+  public int getTurn01() {
+    return turn01;
+  }
+
+  public int getTurn02() {
+    return turn02;
+  }
+
+  // Metodos de estrita alternancia
+
+  public void alternateFrist() {
+    while (getTurn01() != 0) {
+      try {
+        Thread.sleep(1);
+      } catch (InterruptedException e) {
+        System.out.println("Erro!!!!");
+      }
+    }
+  }
+
+  public void alternateSecond() {
+    while (getTurn02() != 1) {
+      try {
+        Thread.sleep(1);
+      } catch (InterruptedException e) {
+        System.out.println("Erro!!!!");
+      }
+    }
+  }
+
+  public String selectedSolution() {
+    return solutionsBox.getSelectionModel().getSelectedItem().toString();
   }
 }
--- threads/ThreadFirstTrain.java
+++ threads/ThreadFirstTrain.java
@@ -1,9 +1,6 @@
 package threads;
 
 import control.mainController;
-import javafx.animation.SequentialTransition;
-import javafx.animation.TranslateTransition;
-import javafx.util.Duration;
 
 public class ThreadFirstTrain extends Thread {
   mainController mainController = new mainController();
@@ -14,184 +11,173 @@ public class ThreadFirstTrain extends Thread {
 
   // Iniciando a thread e gerando os movimentos padrão do trem
 
-  public void run(int pos) {
-    double time = 1500;
-    if (pos == 1) {
-      TranslateTransition translate01 = new TranslateTransition();
-      mainController.setTrainImg("/assets/train.png", 1);
-      translate01.setNode(mainController.getTrain1());
-      translate01.setDuration(Duration.millis(time));
-      translate01.setByX(65);
-      translate01.setByY(18);
-      TranslateTransition translate02 = new TranslateTransition();
-      translate02.setNode(mainController.getTrain1());
-      translate02.setDuration(Duration.millis(time));
-      translate02.setByX(35);
-      translate02.setByY(15);
-      TranslateTransition translate03 = new TranslateTransition();
-      translate03.setNode(mainController.getTrain1());
-      translate03.setDuration(Duration.millis(time));
-      translate03.setByX(50);
-      translate03.setByY(-1.5);
-      TranslateTransition translate04 = new TranslateTransition();
-      translate04.setNode(mainController.getTrain1());
-      translate04.setDuration(Duration.millis(time));
-      translate04.setByX(88);
-      translate04.setByY(-32);
-      TranslateTransition translate05 = new TranslateTransition();
-      translate05.setNode(mainController.getTrain1());
-      translate05.setDuration(Duration.millis(time));
-      translate05.setByX(60);
-      translate05.setByY(0);
-      TranslateTransition translate06 = new TranslateTransition();
-      translate06.setNode(mainController.getTrain1());
-      translate06.setDuration(Duration.millis(time));
-      translate06.setByX(88);
-      translate06.setByY(25);
-      TranslateTransition translate07 = new TranslateTransition();
-      translate07.setNode(mainController.getTrain1());
-      translate07.setDuration(Duration.millis(time));
-      translate07.setByX(60);
-      translate07.setByY(0);
-      TranslateTransition translate08 = new TranslateTransition();
-      translate08.setNode(mainController.getTrain1());
-      translate08.setDuration(Duration.millis(time));
-      translate08.setByX(110);
-      translate08.setByY(-30);
-      SequentialTransition st = new SequentialTransition(translate01, translate02, translate03, translate04, translate05, translate06, translate07, translate08);
-      st.play();
-    } else if (pos == 2) {
-      TranslateTransition translate01 = new TranslateTransition();
-      mainController.setTrainImg("/assets/train.png", 1);
-      translate01.setNode(mainController.getTrain3());
-      translate01.setDuration(Duration.millis(time));
-      translate01.setByX(-65);
-      translate01.setByY(18);
-      TranslateTransition translate02 = new TranslateTransition();
-      translate02.setNode(mainController.getTrain3());
-      translate02.setDuration(Duration.millis(time));
-      translate02.setByX(-38);
-      translate02.setByY(15);
-      TranslateTransition translate03 = new TranslateTransition();
-      translate03.setNode(mainController.getTrain3());
-      translate03.setDuration(Duration.millis(time));
-      translate03.setByX(-50);
-      translate03.setByY(-1.5);
-      TranslateTransition translate04 = new TranslateTransition();
-      translate04.setNode(mainController.getTrain3());
-      translate04.setDuration(Duration.millis(time));
-      translate04.setByX(-88);
-      translate04.setByY(-32);
-      TranslateTransition translate05 = new TranslateTransition();
-      translate05.setNode(mainController.getTrain3());
-      translate05.setDuration(Duration.millis(time));
-      translate05.setByX(-60);
-      translate05.setByY(0);
-      TranslateTransition translate06 = new TranslateTransition();
-      translate06.setNode(mainController.getTrain3());
-      translate06.setDuration(Duration.millis(time));
-      translate06.setByX(-88);
-      translate06.setByY(25);
-      TranslateTransition translate07 = new TranslateTransition();
-      translate07.setNode(mainController.getTrain3());
-      translate07.setDuration(Duration.millis(time));
-      translate07.setByX(-60);
-      translate07.setByY(0);
-      TranslateTransition translate08 = new TranslateTransition();
-      translate08.setNode(mainController.getTrain3());
-      translate08.setDuration(Duration.millis(time));
-      translate08.setByX(-110);
-      translate08.setByY(-30);
-      SequentialTransition st = new SequentialTransition(translate01, translate02, translate03, translate04, translate05, translate06, translate07, translate08);
-      st.play();
-    } else if (pos == 3) {
-      TranslateTransition translate01 = new TranslateTransition();
-      mainController.setTrainImg("/assets/train.png", 1);
-      translate01.setNode(mainController.getTrain1());
-      translate01.setDuration(Duration.millis(time));
-      translate01.setByX(65);
-      translate01.setByY(18);
-      TranslateTransition translate02 = new TranslateTransition();
-      translate02.setNode(mainController.getTrain1());
-      translate02.setDuration(Duration.millis(time));
-      translate02.setByX(35);
-      translate02.setByY(15);
-      TranslateTransition translate03 = new TranslateTransition();
-      translate03.setNode(mainController.getTrain1());
-      translate03.setDuration(Duration.millis(time));
-      translate03.setByX(50);
-      translate03.setByY(-1.5);
-      TranslateTransition translate04 = new TranslateTransition();
-      translate04.setNode(mainController.getTrain1());
-      translate04.setDuration(Duration.millis(time));
-      translate04.setByX(88);
-      translate04.setByY(-32);
-      TranslateTransition translate05 = new TranslateTransition();
-      translate05.setNode(mainController.getTrain1());
-      translate05.setDuration(Duration.millis(time));
-      translate05.setByX(60);
-      translate05.setByY(0);
-      TranslateTransition translate06 = new TranslateTransition();
-      translate06.setNode(mainController.getTrain1());
-      translate06.setDuration(Duration.millis(time));
-      translate06.setByX(88);
-      translate06.setByY(25);
-      TranslateTransition translate07 = new TranslateTransition();
-      translate07.setNode(mainController.getTrain1());
-      translate07.setDuration(Duration.millis(time));
-      translate07.setByX(60);
-      translate07.setByY(0);
-      TranslateTransition translate08 = new TranslateTransition();
-      translate08.setNode(mainController.getTrain1());
-      translate08.setDuration(Duration.millis(time));
-      translate08.setByX(110);
-      translate08.setByY(-30);
-      SequentialTransition st = new SequentialTransition(translate01, translate02, translate03, translate04, translate05, translate06, translate07, translate08);
-      st.play();
-    } else if (pos == 4) {
-      TranslateTransition translate01 = new TranslateTransition();
-      mainController.setTrainImg("/assets/train.png", 1);
-      translate01.setNode(mainController.getTrain2());
-      translate01.setDuration(Duration.millis(time));
-      translate01.setByX(65);
-      translate01.setByY(18);
-      TranslateTransition translate02 = new TranslateTransition();
-      translate02.setNode(mainController.getTrain2());
-      translate02.setDuration(Duration.millis(time));
-      translate02.setByX(35);
-      translate02.setByY(15);
-      TranslateTransition translate03 = new TranslateTransition();
-      translate03.setNode(mainController.getTrain2());
-      translate03.setDuration(Duration.millis(time));
-      translate03.setByX(50);
-      translate03.setByY(-1.5);
-      TranslateTransition translate04 = new TranslateTransition();
-      translate04.setNode(mainController.getTrain2());
-      translate04.setDuration(Duration.millis(time));
-      translate04.setByX(88);
-      translate04.setByY(-32);
-      TranslateTransition translate05 = new TranslateTransition();
-      translate05.setNode(mainController.getTrain2());
-      translate05.setDuration(Duration.millis(time));
-      translate05.setByX(60);
-      translate05.setByY(0);
-      TranslateTransition translate06 = new TranslateTransition();
-      translate06.setNode(mainController.getTrain2());
-      translate06.setDuration(Duration.millis(time));
-      translate06.setByX(88);
-      translate06.setByY(25);
-      TranslateTransition translate07 = new TranslateTransition();
-      translate07.setNode(mainController.getTrain2());
-      translate07.setDuration(Duration.millis(time));
-      translate07.setByX(60);
-      translate07.setByY(0);
-      TranslateTransition translate08 = new TranslateTransition();
-      translate08.setNode(mainController.getTrain2());
-      translate08.setDuration(Duration.millis(time));
-      translate08.setByX(110);
-      translate08.setByY(-30);
-      SequentialTransition st = new SequentialTransition(translate01, translate02, translate03, translate04, translate05, translate06, translate07, translate08);
-      st.play();
+  public void run() {
+    while (true) {
+      switch (mainController.getOp()) {
+        case 1: {
+          mainController.moveX("red", 100, 0, 1);
+          mainController.rotate("red", 45, 1);
+          mainController.moveY("red", 30, 0, "down", 1);
+          while (mainController.getLV1() == 1) {
+            try {
+              Thread.sleep(1);
+              mainController.enterFirstBridge("red");
+            } catch (InterruptedException e) {
+            }
+          }
+          mainController.setLV1(1);
+          mainController.rotate("red", 0, 1);
+          mainController.moveY("red", 52, 30, "down", 1);
+          mainController.moveX("red", 216, 111, 1);
+          mainController.setLV1(0);
+          mainController.rotate("red", 45, 1);
+          mainController.moveY("red", 95, 52, "down", 1);
+          mainController.rotate("red", 0, 1);
+          mainController.moveX("red", 350, 216, 1);
+          mainController.rotate("red", -45, 1);
+          // Zona Critica 2
+          while (mainController.getLV2() == 1) {
+            try {
+              Thread.sleep(1);
+              mainController.enterSecondBridge("red");
+            } catch (InterruptedException e) {
+            }
+          }
+          mainController.setLV2(1);
+          mainController.moveY("red", 95, 52, "up", 1);
+          mainController.rotate("red", 0, 1);
+          mainController.moveX("red", 470, 350, 1);
+          mainController.setLV2(0);
+          mainController.rotate("red", -45, 1);
+          mainController.moveY("red", 30, 0, "up", 1);
+          mainController.rotate("red", 0, 1);
+          mainController.moveX("red", 560, 470, 1);
+          break;
+        }
+        case 2: {
+          mainController.moveX("red", 0, 100, 2);
+          mainController.rotate("red", -45, 2);
+          mainController.moveY("red", 30, 0, "down", 2);
+          // Zona Critica 1
+          while (mainController.getLV1() == 1) {
+            try {
+              Thread.sleep(1);
+              mainController.enterFirstBridge("red");
+            } catch (InterruptedException e) {
+            }
+          }
+          mainController.setLV1(1);
+          mainController.rotate("red", 0, 2);
+          mainController.moveY("red", 52, 30, "down", 2);
+          mainController.moveX("red", 111, 216, 2);
+          mainController.setLV1(0);
+          mainController.rotate("red", -45, 2);
+          mainController.moveY("red", 95, 52, "down", 2);
+          mainController.rotate("red", 0, 2);
+          mainController.moveX("red", 216, 350, 2);
+          mainController.rotate("red", 45, 2);
+          // Zona Critica 2
+          while (mainController.getLV2() == 1) {
+            try {
+              Thread.sleep(1);
+              mainController.enterSecondBridge("red");
+            } catch (InterruptedException e) {
+            }
+          }
+          mainController.setLV2(1);
+          mainController.moveY("red", 95, 52, "up", 2);
+          mainController.rotate("red", 0, 2);
+          mainController.moveX("red", 350, 470, 2);
+          mainController.setLV2(0);
+          mainController.rotate("red", 45, 2);
+          mainController.moveY("red", 30, 0, "up", 2);
+          mainController.rotate("red", 0, 2);
+          mainController.moveX("red", 470, 560, 2);
+          break;
+        }
+        case 3: {
+          mainController.moveX("red", 100, 0, 1);
+          mainController.rotate("red", 45, 1);
+          mainController.moveY("red", 30, 0, "down", 1);
+          // Zona Critica 1
+          while (mainController.getLV1() == 1) {
+            try {
+              Thread.sleep(1);
+              mainController.enterFirstBridge("red");
+            } catch (InterruptedException e) {
+            }
+          }
+          mainController.setLV1(1);
+          mainController.rotate("red", 0, 1);
+          mainController.moveY("red", 52, 30, "down", 1);
+          mainController.moveX("red", 216, 111, 1);
+          mainController.setLV1(0);
+          mainController.rotate("red", -45, 1);
+          mainController.moveY("red", -2, 52, "up", 1);
+          mainController.rotate("red", 0, 1);
+          mainController.moveX("red", 350, 216, 1);
+          mainController.rotate("red", 45, 1);
+          // Zona Critica 2
+          while (mainController.getLV2() == 1) {
+            try {
+              Thread.sleep(1);
+              mainController.enterSecondBridge("red");
+            } catch (InterruptedException e) {
+            }
+          }
+          mainController.setLV2(1);
+          mainController.moveY("red", 44, -2, "down", 1);
+          mainController.rotate("red", 0, 1);
+          mainController.moveX("red", 470, 350, 1);
+          mainController.setLV2(0);
+          mainController.rotate("red", -45, 1);
+          mainController.moveY("red", 30, 0, "up", 1);
+          mainController.rotate("red", 0, 1);
+          mainController.moveX("red", 560, 470, 1);
+          break;
+        }
+        case 4: {
+          mainController.moveX("red", 0, 100, 2);
+          mainController.rotate("red", -45, 2);
+          mainController.moveY("red", 30, 0, "down", 2);
+          // Zona Critica 1
+          while (mainController.getLV2() == 1) {
+            try {
+              Thread.sleep(1);
+              mainController.enterSecondBridge("red");
+            } catch (InterruptedException e) {
+            }
+          }
+          mainController.setLV2(1);
+          mainController.rotate("red", 0, 2);
+          mainController.moveY("red", 52, 30, "down", 2);
+          mainController.moveX("red", 111, 216, 2);
+          mainController.setLV2(0);
+          mainController.rotate("red", -45, 2);
+          mainController.moveY("red", 95, 52, "down", 2);
+          mainController.rotate("red", 0, 2);
+          mainController.moveX("red", 216, 350, 2);
+          mainController.rotate("red", 45, 2);
+          // Zona Critica 2
+          while (mainController.getLV1() == 1) {
+            try {
+              Thread.sleep(1);
+              mainController.enterFirstBridge("red");
+            } catch (InterruptedException e) {
+            }
+          }
+          mainController.setLV1(1);
+          mainController.moveY("red", 95, 52, "up", 2);
+          mainController.rotate("red", 0, 2);
+          mainController.moveX("red", 350, 470, 2);
+          mainController.setLV1(0);
+          mainController.rotate("red", 45, 2);
+          mainController.moveY("red", 30, 0, "up", 2);
+          mainController.rotate("red", 0, 2);
+          mainController.moveX("red", 470, 560, 2);
+          break;
+        }
+      }
     }
   }
 }
